c72d1c61636c5ee85dd6158078274a1f4237ad62
WIN_NT\x16j029 <x16j029@chiba-fjb.ac.jp>
2017-04-28 11:52:11 +0900

    コミット

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace Example2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = String.Format("{0}", 1 + 2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label6.Text = String.Format("{0}", 5-3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label7.Text = String.Format("{0}", 2 * 3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label8.Text = String.Format("{0}", 8 / 2);
+        }
     }
 }
